ed15e70adc97625ce61646873b11f53279cafd75
Ryan Priebe <ryan@wearefree.ca>
2014-02-12 15:17:22 -0700

    added class names to namespaces

--- electroheart.php
+++ electroheart.php
@@ -1,6 +1,6 @@
 <?php
 	
-	namespace SpaceLord;
+	namespace SpaceLord\Electroheart;
 
 	/**
 	 * Electroheart
--- status.php
+++ status.php
@@ -1,6 +1,6 @@
 <?php
 	
-	namespace SpaceLord;
+	namespace SpaceLord\Status;
 
 	/**
 	 * Status
